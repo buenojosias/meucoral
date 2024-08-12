72d97a4d66798c39ad73317ac67223e0b0a8eeb0
Josias Pereira Bueno <66849846+buenojosias@users.noreply.github.com>
2024-08-11 23:29:51 -0300

    Correção na UserSeeder do release 0.1

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -20,7 +20,7 @@ class UserSeeder extends Seeder
             'role' => 'admin',
         ]);
 
-        $users = User::factory()->create([
+        User::factory()->create([
             'plan_id' => 3,
             'name' => 'Josias Bueno',
             'email' => 'josias.jpb@gmail.com',
@@ -32,26 +32,26 @@ class UserSeeder extends Seeder
         //     'role' => 'manager'
         // ]);
 
-        $users->each(
-            function ($user) {
-                if ($user->plan_id > 1) {
-                    $payment_cycle = \Arr::random(['Mensal', 'Anual']);
-                    $last_payment = Carbon::now()->subDays(rand(0, 20));
-                    $next_payment = $payment_cycle === 'Mensal' ? $last_payment . '+ 1 month' : $last_payment . '+ 1 year';
-                } else {
-                    $payment_cycle = null;
-                }
-                $data = [
-                    'plan_cost' => $user->plan->price,
-                    'payment_cycle' => $payment_cycle,
-                    'discount' => $payment_cycle === 'Anual' ? ($user->plan->price * 2) : 0,
-                    'final_cost' => $payment_cycle === 'Anual' ? ($user->plan->price * 10) : $user->plan->price,
-                    'last_payment' => $last_payment ?? null,
-                    'next_payment' => $next_payment ?? null,
-                    'status' => 'Ativo'
-                ];
-                $user->config()->create($data);
-            }
-        );
+        // $users->each(
+        //     function ($user) {
+        //         if ($user->plan_id > 1) {
+        //             $payment_cycle = \Arr::random(['Mensal', 'Anual']);
+        //             $last_payment = Carbon::now()->subDays(rand(0, 20));
+        //             $next_payment = $payment_cycle === 'Mensal' ? $last_payment . '+ 1 month' : $last_payment . '+ 1 year';
+        //         } else {
+        //             $payment_cycle = null;
+        //         }
+        //         $data = [
+        //             'plan_cost' => $user->plan->price,
+        //             'payment_cycle' => $payment_cycle,
+        //             'discount' => $payment_cycle === 'Anual' ? ($user->plan->price * 2) : 0,
+        //             'final_cost' => $payment_cycle === 'Anual' ? ($user->plan->price * 10) : $user->plan->price,
+        //             'last_payment' => $last_payment ?? null,
+        //             'next_payment' => $next_payment ?? null,
+        //             'status' => 'Ativo'
+        //         ];
+        //         $user->config()->create($data);
+        //     }
+        // );
     }
 }
